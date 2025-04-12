0302c33a213da7db10e6f7e704be717af65f21e8
Adaviladev <alvarodaviladev@gmail.com>
2025-04-11 21:00:55 -0400

    Set up App component and project structure

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.module.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Jammming</h1>
+      <p>Discover and create playlists</p>
+    </div>
+  );
+}
+
+export default App;
